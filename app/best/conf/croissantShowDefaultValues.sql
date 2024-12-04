2ccfa47b3e618a3975571533be558d3244e465b4
bverrier <bastien.verrier@outlook.fr>
2024-12-04 10:29:24 +0100

    first commit

--- app/best/conf/croissantShowDefaultValues.sql
+++
@@ -1,6 +0,0 @@
-use croissantShow;
-
---
--- Create Admin
---
-INSERT INTO `users` (`prenom`, `nom`, `mail`, `login`, `is_admin`, `is_activ`, `date_creation_compte`, `mot_de_passe`, `croissant_buy`) VALUES ( 'root', 'root', 'root@root.fr', 'root', '1', '1', current_timestamp(), '$2y$10$7aZrDGDTbhEiUf9NAYfs2uJNoARVHCI/Yxf1O3SGQmjBTAEm0LuS.', '0');
